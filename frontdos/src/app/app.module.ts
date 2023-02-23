88956c081420656c849b9c365638eceb5bc86973
richardfffff <richardmace6@gmail.com>
2023-02-23 15:05:17 -0300

    se introdujo la parte de login y se acoplo con el back. todo ok

--- frontdos/src/app/app.module.ts
+++ frontdos/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { interceptorProvider } from './service/interceptor-service';
 
 @NgModule({
   declarations: [
@@ -41,7 +42,9 @@ import { LoginComponent } from './components/login/login.component';
     NgCircleProgressModule.forRoot({}),
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
